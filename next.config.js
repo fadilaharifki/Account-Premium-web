cc58cd9ff2736031aea7f05e6e01e71e122f3374
Fadiah Arifki <engineer@Mac-mini-engineer.local>
2023-11-27 21:40:12 +0700

    product

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.google.com", "unsplash.com", "images.unsplash.com", "buffer.com"], // Add the required domains here
+    domains: ["www.google.com", "unsplash.com", "images.unsplash.com", "buffer.com", "ik.imagekit.io"], // Add the required domains here
   },
 };
 
